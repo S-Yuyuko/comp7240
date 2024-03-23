10c2381e33461a2cfb7c1be7cda533fe7f84608e
S-Yuyuko <wz1305290174@gmail.com>
2024-03-23 14:58:15 +0800

    modify the position

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import MovieGenres from './MovieGenres';
+import MovieGenres from './component/MovieGenres';
 
 function App() {
   const [showModal, setShowModal] = useState(true);
